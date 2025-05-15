0497a45126f970a35467f54459eedb7265d30239
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-05-15 20:58:00 +0200

    Update README.md

--- Program.cs
+++ Program.cs
@@ -16,56 +16,43 @@
 // </summary>
 //-----------------------------------------------------------------------
 
-/* Imports from NET Framework */
-using System;
-
 namespace Console.ExpSoftware
 {
+    /* Imports from NET Framework */
+    using System;
+
     public class Program
     {
         private static void Main(string[] args)
         {
+            ConsoleMenu.Add("1", "Auswahl Menüpunkt 1", () => MenuPoint1());
+            ConsoleMenu.Add("2", "Auswahl Menüpunkt 2", () => MenuPoint2());
+            ConsoleMenu.Add("X", "Beenden", () => ApplicationExit());
+
             do
             {
-                Console.Clear();
-                Console.WriteLine("1. Menüpunkt 1");
-                Console.WriteLine("2. Menüpunkt 2");
-                Console.WriteLine("X. Beenden");
-                Console.WriteLine("Wählen Sie einen Menüpunkt oder 'x' für beenden");
-                ConsoleKey key = Console.ReadKey(true).Key;
-                if (key == ConsoleKey.X)
-                {
-                    Environment.Exit(0);
-                }
-                else
-                {
-                    if (key == ConsoleKey.D1)
-                    {
-                        MenuPoint1();
-                    }
-                    else if (key == ConsoleKey.D2)
-                    {
-                        MenuPoint2();
-                    }
-                }
+                _ = ConsoleMenu.SelectKey(2, 2);
             }
             while (true);
+        }
+
+        private static void ApplicationExit()
+        {
+            Environment.Exit(0);
         }
 
         private static void MenuPoint1()
         {
             Console.Clear();
 
-            Console.WriteLine("Mit einer beliebigen Taste zurück zum Menü!");
-            Console.ReadKey();
+            ConsoleMenu.Wait();
         }
 
         private static void MenuPoint2()
         {
             Console.Clear();
 
-            Console.WriteLine("Mit einer beliebigen Taste zurück zum Menü!");
-            Console.ReadKey();
+            ConsoleMenu.Wait();
         }
     }
 }
